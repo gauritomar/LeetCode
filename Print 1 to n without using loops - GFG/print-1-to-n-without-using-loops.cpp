c5272ad02c957a754822ebc9add0ce7a4b3d79f1
Gauri <97106972+gauritomar@users.noreply.github.com>
2022-12-10 08:16:53 +0530

    Added solution - LeetHub

---
+++ Print 1 to n without using loops - GFG/print-1-to-n-without-using-loops.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    void printTillN(int N)
+    {
+        int i = 1;
+ 
+    while (i <= N) {
+        cout << i++ << " ";
+    }
+    
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        ob.printTillN(N);
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
